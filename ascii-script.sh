5a6e6c55ec6c355998c1f5aefd9366ef78069f24
adeyoyg <olawaleoyg@gmail.com>
2024-08-05 22:40:32 +0100

    add script

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,11 +1,5 @@
-    - name: Install Cowsay Program
-      run: sudo apt-get install cowsay -y
-
-    - name: Execute cowsay CMD
-      run: cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
-
-    - name: Test File Exists
-      run: grep -i "dragon" dragon.txt
-
-    - name: Read file
-      run: cat dragon.txt
+#!/bin/sh
+sudo apt-get install cowsay -y
+cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
+grep -i "dragon" dragon.txt
+cat dragon.txt
